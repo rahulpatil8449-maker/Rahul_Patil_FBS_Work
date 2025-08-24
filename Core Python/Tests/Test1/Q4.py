6d108a9a0336e1bc03b758af47086392cac303e5
rahulpatil8449-maker <rahulpatil8449@gmail.com>
2025-08-24 18:10:22 +0530

    Q4.py

--- Core Python/Tests/Test1/Q4.py
+++ Core Python/Tests/Test1/Q4.py
@@ -1,8 +1,4 @@
-l = int(input("Enter the length of the wall:"))
-b = int(input("Enter the breadth of the wall:"))
-
-area = l * b
-print("The area of one wall will be:", area)
+area = int(input("The area of one wall is:"))
 
 cost_int = int(input("Enter the interior painting cost per unit area: "))
 cost_ext = int(input("Enter the exterior painting cost per unit area: "))
@@ -10,8 +6,9 @@ cost_ext = int(input("Enter the exterior painting cost per unit area: "))
 interior_cost = 8 * area * cost_int
 print("The cost for the whole interior area of the wall is:", interior_cost)
 
-exterior_cost = 6 * area * cost_ext
+exterior_cost = 7 * area * cost_ext
 print("The cost for the whole exterior area of the wall is:", exterior_cost)
 
 total_cost = interior_cost + exterior_cost
 print("The Total cost will be:", total_cost)
+
