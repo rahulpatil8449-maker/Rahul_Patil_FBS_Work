68b6a63ac0fe4e7bd1377d34a9247418a4a84c92
rahulpatil8449-maker <rahulpatil8449@gmail.com>
2025-09-01 10:16:25 +0530

    Add files via upload

---
+++ Core Python/Assignments/Assignment1/Q7.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+
+r1 = -b + ((b ** 2 - 4 * a * c)) * 0.5
+r2 = -b - ((b ** 2 - 4 * a * c)) * 0.5
+
+print("The Quadratic Equation value of r1 will be:", r1)
+print("The Quadratic Equation value of r2 will be:", r2)
